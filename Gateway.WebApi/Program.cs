bbcb41c27dd8dc869aae2655b60a110ac800369d
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2021-11-21 01:36:20 +0200

    configure API gateway

--- Gateway.WebApi/Program.cs
+++ Gateway.WebApi/Program.cs
@@ -1,11 +1,11 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Gateway.WebApi
 {
@@ -16,11 +16,17 @@ namespace Gateway.WebApi
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        })
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+    });
     }
 }
